f855e9b4737813ab2f09b5052b6bb937e82d0c07
Manoj Prabhakar <programmingwithmano@gmail.com>
2021-12-07 00:03:57 +0530

    Updates

---
+++ precache-manifest.0e4a89c29d1dc36570692cdcc21db481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4df31980adf9cd5871177039966139d",
+    "url": "/quickmart-survey/index.html"
+  },
+  {
+    "revision": "4ec4e939bcbac8a5db0f",
+    "url": "/quickmart-survey/static/css/main.3ed2150e.chunk.css"
+  },
+  {
+    "revision": "19d689b4af41abbf7243",
+    "url": "/quickmart-survey/static/js/2.1ed5c7d2.chunk.js"
+  },
+  {
+    "revision": "76f365cdeeb4b59c42a5be7381ee59a9",
+    "url": "/quickmart-survey/static/js/2.1ed5c7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec4e939bcbac8a5db0f",
+    "url": "/quickmart-survey/static/js/main.af16a977.chunk.js"
+  },
+  {
+    "revision": "218e3c1daa946467106f",
+    "url": "/quickmart-survey/static/js/runtime-main.380c805c.js"
+  }
+]);
