94102be9a35b8b56bd6076f85b4b19552386fbbb
programmingwithmano <60712135+programmingwithmano@users.noreply.github.com>
2021-12-06 19:04:49 +0000

    Deploying to gh-pages from @ programmingwith-mano/QuickMart-Survey@07e6d0a882493a309f704b2760e497cdbddcc7e9 🚀

---
+++ precache-manifest.80a22cf964400f6595951d6d5ca89b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1947b08c25509d1e6c9c48ac35ff5406",
+    "url": "/QuickMart-Survey/index.html"
+  },
+  {
+    "revision": "1d7562831e6fff137e2e",
+    "url": "/QuickMart-Survey/static/css/main.1821182d.chunk.css"
+  },
+  {
+    "revision": "c240ee21d4760ea09a09",
+    "url": "/QuickMart-Survey/static/js/2.7e720c78.chunk.js"
+  },
+  {
+    "revision": "76f365cdeeb4b59c42a5be7381ee59a9",
+    "url": "/QuickMart-Survey/static/js/2.7e720c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d7562831e6fff137e2e",
+    "url": "/QuickMart-Survey/static/js/main.1c742239.chunk.js"
+  },
+  {
+    "revision": "d0759ebfcd78acc1d317",
+    "url": "/QuickMart-Survey/static/js/runtime-main.59f01a4a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b84e86cad8aafdb4c34f0b7968f4e10.js"
+  "/QuickMart-Survey/precache-manifest.80a22cf964400f6595951d6d5ca89b1a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/QuickMart-Survey/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
